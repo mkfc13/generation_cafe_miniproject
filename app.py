13c46fe94501bf7daf517bc5fa6eff55efc0e0a9
mkfc13 <mkfc13@gmail.com>
2022-05-19 15:16:27 +0100

    Refactor mini6

--- app.py
+++ app.py
@@ -1,11 +1,77 @@
 from functions.function import *
-from functions.product import *
-from functions.courier import *
-from functions.order import *
-from functions.option import *
+from functions.logo import *
 import pymysql
 
-# Make it pretty with pandas after everything works.
+
+def selected_menu(choice):
+    while True:
+        if choice == 1:
+            print_product_menu_message()
+        elif choice == 2:
+            print_courier_menu_message()
+        elif choice == 3:
+            print_order_menu_message()
+        else:
+            print("You shouldn't be here\n")
+
+        num = input()
+        print('................................................................\n')
+
+        if num == '0':
+            print_main_menu_logo()
+            break
+
+        elif num == '1':
+            select_table(choice)
+            print('................................................................\n')
+
+        elif num == '2':
+            create_new_row(choice)
+            print('................................................................\n')
+
+        elif num == '3':
+            update_row(choice)
+            print('................................................................\n')
+
+        elif num == '4':
+            delete_row(choice)
+            print('................................................................\n')
+
+        else:
+            print('Please choose only the following options:\n')
+
+
+def order_menu(choice):
+    while True:
+        print_order_menu_message()
+        num = input()
+        print('................................................................\n')
+
+        if num == '0':
+            print_main_menu_logo()
+            break
+
+        elif num == '1':
+            select_table(choice)
+            print('................................................................\n')
+
+        elif num == '2':
+            create_new_order()
+            print('................................................................\n')
+
+        elif num == '3':
+            update_order_status()
+            print('................................................................\n')
+
+        elif num == '4':
+            update_row(choice)
+            print('................................................................\n')
+
+        elif num == '5':
+            delete_row(choice)
+            print('................................................................\n')
+        else:
+            print('Please choose only the following options:\n')
 
 
 print_cafe_logo()
@@ -13,7 +79,6 @@ print_main_menu_logo()
 while True:
     print_main_menu_message()
     choice = input()
-
     if choice == '0':
         print_exit_message()
         cursor.close()
@@ -21,11 +86,11 @@ while True:
         break
     elif choice == '1':
         print_product_menu_logo()
-        product_menu(int(choice))
+        selected_menu(int(choice))
     elif choice == '2':
         current_menu = 2
         print_courier_menu_logo()
-        courier_menu(int(choice))
+        selected_menu(int(choice))
     elif choice == '3':
         print_order_menu_logo()
         order_menu(int(choice))
